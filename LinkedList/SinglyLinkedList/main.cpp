1f064487ca1c6fd5b129826fd82b0c377fa9075f
Jaehyun Ko <jaehyun.dev@gmail.com>
2021-02-07 21:15:26 +0900

    add test case - mem leak

--- LinkedList/SinglyLinkedList/main.cpp
+++ LinkedList/SinglyLinkedList/main.cpp
@@ -1,50 +1,57 @@
 #include <cassert>>
-#include "SinglyLinkedList.h"
+#include "SinglyLinkedlist.h"
 
 int main()
 {
-	jh::SinglyLinkedList list;
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	assert(list.Size() == 0);
+	jh::SinglyLinkedList* list = new jh::SinglyLinkedList();
 
-	list.Append(5);
-	assert(list.Size() == 1);
-	assert(list.ToVector()[0] == 5);
+	assert(list->Size() == 0);
 
-	list.Append(3);
-	assert(list.Size() == 2);
-	assert(list.ToVector()[0] == 5);
-	assert(list.ToVector()[1] == 3);
+	list->Append(5);
+	assert(list->Size() == 1);
+	assert(list->ToVector()[0] == 5);
 
-	list.Append(1);
-	list.Remove(3);
-	assert(list.Size() == 2);
-	assert(list.ToVector()[0] == 5);
-	assert(list.ToVector()[1] == 1);
+	list->Append(3);
+	assert(list->Size() == 2);
+	assert(list->ToVector()[0] == 5);
+	assert(list->ToVector()[1] == 3);
 
-	list.Remove(5);
-	assert(list.Size() == 1);
-	assert(list.ToVector()[0] == 1);
+	list->Append(1);
+	list->Remove(3);
+	assert(list->Size() == 2);
+	assert(list->ToVector()[0] == 5);
+	assert(list->ToVector()[1] == 1);
 
-	list.Remove(9);
-	assert(list.Size() == 1);
-	assert(list.ToVector()[0] == 1);
+	list->Remove(5);
+	assert(list->Size() == 1);
+	assert(list->ToVector()[0] == 1);
 
-	list.Remove(1);
-	assert(list.Size() == 0);
-	assert(list.ToVector().size() == 0);
+	list->Remove(9);
+	assert(list->Size() == 1);
+	assert(list->ToVector()[0] == 1);
+
+	list->Remove(1);
+	assert(list->Size() == 0);
+	assert(list->ToVector().size() == 0);
 
 	for (int i = 0; i < 5; ++i)
 	{
-		list.Append(i);
-		assert(list.Size() == (i + 1));
+		list->Append(i);
+		assert(list->Size() == (i + 1));
 	}
 
 	for (int i = 0; i < 5; ++i)
 	{
-		list.Remove(i);
-		assert(list.Size() == (5 - i - 1));
+		list->Remove(i);
+		assert(list->Size() == (5 - i - 1));
 	}
+
+	list->Append(0);
+	list->Append(1);
+
+	delete list;
 
 	return 0;
 }
